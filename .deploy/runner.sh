65540bd6e6067943e4067f38ed2618f85569e3e9
0chil <0@chll.it>
2023-06-15 18:48:59 +0900

    chore(cd): initialize workflow

---
+++ .deploy/runner.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+JAR_NAME="snackgame-server.jar"
+PROCESS_ID=$(pgrep -f "$JAR_NAME")
+PATH="/home/ubuntu/snackgame-server"
+
+if [ -n "$PROCESS_ID" ]; then
+  sudo kill $PROCESS_ID
+  echo "\n🐣 구동중인 애플리케이션을 종료했습니다. (pid : $PROCESS_ID)\n"
+fi
+
+echo "\n🐣 SpringBoot 애플리케이션을 실행합니다.\n"
+
+nohup java -jar $PATH/$JAR_NAME --server.port=8081 > $PATH/spring.log &
+
+exit
