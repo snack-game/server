4f1d2ad47469aa572cb23a2477455b21d91eeea0
0chil <0@chll.it>
2023-06-24 14:55:40 +0900

    실행 스크립트 오류 개선 (#3)

--- .deploy/runner.sh
+++ .deploy/runner.sh
@@ -2,15 +2,15 @@
 
 JAR_NAME="snackgame-server.jar"
 PROCESS_ID=$(pgrep -f "$JAR_NAME")
-PATH="/home/ubuntu/snackgame-server"
+APP_PATH="/home/ubuntu/snackgame-server"
 
 if [ -n "$PROCESS_ID" ]; then
-  sudo kill $PROCESS_ID
+  sudo kill -9 $PROCESS_ID
   echo "\n🐣 구동중인 애플리케이션을 종료했습니다. (pid : $PROCESS_ID)\n"
 fi
 
 echo "\n🐣 SpringBoot 애플리케이션을 실행합니다.\n"
 
-nohup java -jar $PATH/$JAR_NAME -DSpring.profiles.active=production > $PATH/spring.log &
+nohup java -jar $APP_PATH/$JAR_NAME -DSpring.profiles.active=production > $APP_PATH/spring.log &
 
 exit
