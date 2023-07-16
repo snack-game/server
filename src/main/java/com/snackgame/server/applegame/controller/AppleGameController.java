6393b502fa610b818c60418e37ab80a35d03d80b
0chil <0@chll.it>
2023-07-17 03:07:56 +0900

    docs: 문서에 인증 추가

--- src/main/java/com/snackgame/server/applegame/controller/AppleGameController.java
+++ src/main/java/com/snackgame/server/applegame/controller/AppleGameController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.snackgame.server.applegame.business.AppleGameService;
@@ -14,6 +15,7 @@ import com.snackgame.server.applegame.controller.dto.AppleGameResponse;
 import com.snackgame.server.applegame.controller.dto.MoveRequest;
 import com.snackgame.server.member.business.domain.Member;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -22,23 +24,27 @@ public class AppleGameController {
 
     private final AppleGameService appleGameService;
 
+    @SecurityRequirement(name = "jwtAuth")
     @PostMapping("/games/1")
     public AppleGameResponse startGameFor(Member member) {
         AppleGame game = appleGameService.startGameOf(member);
         return AppleGameResponse.of(game);
     }
 
+    @SecurityRequirement(name = "jwtAuth")
     @PutMapping("/sessions/{sessionId}/moves")
-    public void placeMoves(Member member, @PathVariable Long sessionId, List<MoveRequest> moves) {
+    public void placeMoves(Member member, @PathVariable Long sessionId, @RequestBody List<MoveRequest> moves) {
         appleGameService.placeMoves(member, sessionId, moves);
     }
 
+    @SecurityRequirement(name = "jwtAuth")
     @DeleteMapping("/sessions/{sessionId}/board")
     public AppleGameResponse resetBoard(Member member, @PathVariable Long sessionId) {
         AppleGame game = appleGameService.resetBoard(member, sessionId);
         return AppleGameResponse.of(game);
     }
 
+    @SecurityRequirement(name = "jwtAuth")
     @PutMapping("/sessions/{sessionId}/end")
     public void endSession(Member member, @PathVariable Long sessionId) {
         appleGameService.endSession(member, sessionId);
