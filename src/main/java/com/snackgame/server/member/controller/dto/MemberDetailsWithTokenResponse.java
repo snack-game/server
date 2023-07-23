3b7ef9ac9e1815c76bb3e1096e57924dde012131
0chil <0@chll.it>
2023-07-23 10:56:11 +0900

    refactor: MemberDetailsResponse를 재활용한다

--- src/main/java/com/snackgame/server/member/controller/dto/MemberDetailsWithTokenResponse.java
+++ src/main/java/com/snackgame/server/member/controller/dto/MemberDetailsWithTokenResponse.java
@@ -2,23 +2,19 @@ package com.snackgame.server.member.controller.dto;
 
 import com.snackgame.server.member.business.domain.Member;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
+@AllArgsConstructor
 @Getter
 public class MemberDetailsWithTokenResponse {
 
-    private final Long id;
-    private final String name;
-    private final GroupResponse group;
-    private final String accessToken;
+    private MemberDetailsResponse member;
+    private String accessToken;
 
     public static MemberDetailsWithTokenResponse of(Member member, String accessToken) {
         return new MemberDetailsWithTokenResponse(
-                member.getId(),
-                member.getName(),
-                GroupResponse.of(member.getGroup()),
+                MemberDetailsResponse.of(member),
                 accessToken
         );
     }
