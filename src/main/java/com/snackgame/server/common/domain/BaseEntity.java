19f34de6b97642df6cdeec46f1937d8e140ad1d5
0chil <0@chll.it>
2023-08-23 23:31:17 +0900

    chore: 프로덕션 환경 DB 연결

--- src/main/java/com/snackgame/server/common/domain/BaseEntity.java
+++ src/main/java/com/snackgame/server/common/domain/BaseEntity.java
@@ -5,7 +5,6 @@ import java.time.LocalDateTime;
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 
-import org.hibernate.annotations.ColumnDefault;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -18,10 +17,8 @@ import lombok.Getter;
 public abstract class BaseEntity {
 
     @CreatedDate
-    @ColumnDefault("CURRENT_TIMESTAMP")
     protected LocalDateTime createdAt;
 
     @LastModifiedDate
-    @ColumnDefault("CURRENT_TIMESTAMP")
     protected LocalDateTime updatedAt;
 }
