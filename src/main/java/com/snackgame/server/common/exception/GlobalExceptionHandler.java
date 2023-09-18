25e3fc95f5df282a97cdb44ef96de08b7de1f8e0
0chil <0@chll.it>
2023-09-18 18:15:55 +0900

    feat: 루트 접근 시 서비스 메인으로 리다이렉션한다 (#46)
    * feat: 루트 접근 시 서비스 메인으로 리다이렉션한다
    
    * refactor: 404 핸들링, 예외 로깅 개선

--- src/main/java/com/snackgame/server/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/snackgame/server/common/exception/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 import com.snackgame.server.auth.exception.AuthorizationException;
 import com.snackgame.server.common.exception.dto.ExceptionResponse;
@@ -26,49 +27,65 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ExceptionResponse handleUnhandled(Exception exception) {
-        logger.error(exception.getMessage(), exception.getCause());
-        if (logger.isDebugEnabled()) {
-            exception.printStackTrace();
-        }
+        logError(exception);
         return ExceptionResponse.withoutMessage();
     }
 
     @ExceptionHandler({AuthorizationException.class, MemberIdNotFoundException.class})
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     public ExceptionResponse handleAuthenticationException(Exception exception) {
-        logger.info(exception.getMessage(), exception.getCause());
+        logWarn(exception);
         return ExceptionResponse.withMessageOf(exception);
+    }
+
+    @ExceptionHandler({NoHandlerFoundException.class})
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ExceptionResponse handleNoHandlerFound(Exception exception) {
+        logDebug(exception);
+        return ExceptionResponse.of("리소스를 찾지 못했습니다");
     }
 
     @ExceptionHandler(BusinessException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handleBusinessException(BusinessException exception) {
-        logger.debug(exception.getMessage(), exception.getCause());
+        logDebug(exception);
         return ExceptionResponse.withMessageOf(exception);
     }
 
     @ExceptionHandler(MissingServletRequestParameterException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handleMissingRequestParameter(MissingServletRequestParameterException exception) {
-        logger.debug(exception.getMessage(), exception.getCause());
+        logDebug(exception);
         return ExceptionResponse.of("쿼리스트링 '" + exception.getParameterName() + "'이 없거나 잘못됐습니다");
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handleHttpMessageNotReadable(HttpMessageNotReadableException exception) {
-        logger.debug(exception.getMessage(), exception.getCause());
+        logDebug(exception);
         return ExceptionResponse.of("요청 페이로드를 읽지 못했습니다");
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ExceptionResponse handleMethodArgumentNotValid(MethodArgumentNotValidException exception) {
-        logger.debug(exception.getMessage(), exception.getCause());
+        logDebug(exception);
         List<String> exceptionMessages = exception.getFieldErrors().stream()
                 .map(DefaultMessageSourceResolvable::getDefaultMessage)
                 .collect(Collectors.toList());
 
         return ExceptionResponse.of(exceptionMessages);
+    }
+
+    private void logWarn(Exception exception) {
+        logger.warn(exception.toString());
+    }
+
+    private void logDebug(Exception exception) {
+        logger.debug(exception.getMessage(), exception);
+    }
+
+    private void logError(Exception exception) {
+        logger.error(exception.getMessage(), exception);
     }
 }
