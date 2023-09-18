33384a47e1d4affc7d1d7df9530498675ef30d15
0chil <0@chll.it>
2023-09-18 15:59:06 +0900

    feat: 루트 접근 시 서비스 메인으로 리다이렉션한다

--- src/main/java/com/snackgame/server/config/WebMvcConfig.java
+++ src/main/java/com/snackgame/server/config/WebMvcConfig.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import com.snackgame.server.auth.BearerTokenExtractor;
@@ -17,6 +18,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+    
+    private static final String HOST_NAME = "snackga.me";
 
     private final JwtProvider jwtProvider;
     private final MemberService memberService;
@@ -31,11 +34,17 @@ public class WebMvcConfig implements WebMvcConfigurer {
     }
 
     @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "https://" + HOST_NAME);
+    }
+
+    @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .exposedHeaders("Location")
                 .allowedOriginPatterns(
-                        "https://*snackga.me",
+                        "https://" + HOST_NAME,
+                        "https://api." + HOST_NAME,
                         "https://*snack-game.vercel.app",
                         "http://localhost:[*]"
                 )
