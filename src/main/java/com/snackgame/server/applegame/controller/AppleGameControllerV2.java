f8d6186b27a9906cbc2059dee8ca18e2169ee491
0chil <0@chll.it>
2023-10-04 21:38:35 +0900

    fix: 응답 코드 수정 및 문서화

--- src/main/java/com/snackgame/server/applegame/controller/AppleGameControllerV2.java
+++ src/main/java/com/snackgame/server/applegame/controller/AppleGameControllerV2.java
@@ -18,29 +18,23 @@ import com.snackgame.server.applegame.controller.dto.AppleGameResponseV2;
 import com.snackgame.server.applegame.controller.dto.RangeRequest;
 import com.snackgame.server.member.business.domain.Member;
 
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v2")
-public class AppleGameControllerV2 {
+public class AppleGameControllerV2 implements AppleGameControllerV2Docs {
 
     private final AppleGameService appleGameService;
 
-    @Operation(summary = "게임 세션 시작", description = "1번 게임(사과게임) 세션을 시작한다")
+    @Override
     @PostMapping("/games/1")
     public AppleGameResponseV2 startGameFor(Member member) {
         AppleGame game = appleGameService.startGameOf(member);
         return AppleGameResponseV2.of(game);
     }
 
-    @Operation(summary = "세션에 수 삽입", description = "지정한 세션에 수들을 삽입한다. 황금사과를 제거한 경우 초기화된 판을 응답한다.", responses = {
-            @ApiResponse(responseCode = "200", description = "판이 초기화되지 않은 경우", content = @Content()),
-            @ApiResponse(responseCode = "205", description = "황금사과로 판이 초기화된 경우")
-    })
+    @Override
     @PutMapping("/sessions/{sessionId}/moves")
     public ResponseEntity<AppleGameResponseV2> placeMoves(
             Member member,
@@ -49,20 +43,20 @@ public class AppleGameControllerV2 {
     ) {
         return appleGameService.placeMoves(member, sessionId, ranges)
                 .map(game -> ResponseEntity
-                        .status(HttpStatus.RESET_CONTENT)
+                        .status(HttpStatus.CREATED)
                         .body(AppleGameResponseV2.of(game))
                 )
                 .orElseGet(() -> ResponseEntity.ok().build());
     }
 
-    @Operation(summary = "게임판 초기화", description = "지정한 세션의 게임판을 초기화한다. 황금사과와는 별도의 기능이다.")
+    @Override
     @DeleteMapping("/sessions/{sessionId}/board")
     public AppleGameResponseV2 resetBoard(Member member, @PathVariable Long sessionId) {
         AppleGame game = appleGameService.resetBoard(member, sessionId);
         return AppleGameResponseV2.of(game);
     }
 
-    @Operation(summary = "게임 세션 종료", description = "현재 세션의 종료를 알린다")
+    @Override
     @PutMapping("/sessions/{sessionId}/end")
     public void endSession(Member member, @PathVariable Long sessionId) {
         appleGameService.endSession(member, sessionId);
