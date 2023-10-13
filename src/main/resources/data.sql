bf5248c7fb1505dd793b477e224910a38b1235aa
0chil <0@chll.it>
2023-10-13 20:49:03 +0900

    refactor: 기본 데이터 삭제

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO MEMBER_GROUP(name) VALUES ('홍천고등학교');
-INSERT INTO MEMBER_GROUP(name) VALUES ('우테코');
-
-INSERT INTO MEMBER(name, group_id) VALUES ('똥수', 1);
-INSERT INTO MEMBER(name, group_id) VALUES ('땡칠', 2);
-INSERT INTO MEMBER(name, group_id) VALUES ('땡칠이', 2);
