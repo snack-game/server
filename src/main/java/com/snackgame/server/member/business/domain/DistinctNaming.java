bfb8a4282de431775ac6b4bfa11fa3cac0d5911a
0chil <0@chll.it>
2023-10-30 01:19:58 +0900

    feat: 중복을 제거한 이름 생성

---
+++ src/main/java/com/snackgame/server/member/business/domain/DistinctNaming.java
@@ -0,0 +1,37 @@
+package com.snackgame.server.member.business.domain;
+
+import org.springframework.stereotype.Component;
+
+import com.snackgame.server.member.business.exception.DuplicateNameException;
+
+@Component
+public class DistinctNaming {
+
+    private final MemberRepository members;
+    private final GuestNameRandomizer guestNameRandomizer = new GuestNameRandomizer();
+
+    public DistinctNaming(MemberRepository members) {
+        this.members = members;
+    }
+
+    public void validate(Name name) {
+        if (members.existsByName(name)) {
+            throw new DuplicateNameException();
+        }
+    }
+
+    public Name ofGuest() {
+        Name name = guestNameRandomizer.get();
+        while (members.existsByName(name)) {
+            name = guestNameRandomizer.get();
+        }
+        return name;
+    }
+
+    public Name from(Name name) {
+        while (members.existsByName(name)) {
+            name = name.nextAvailable();
+        }
+        return name;
+    }
+}
