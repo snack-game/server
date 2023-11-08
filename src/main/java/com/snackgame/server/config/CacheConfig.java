71e46e83f32ada360be23f898a8f33902b1e19b1
0chil <0@chll.it>
2023-11-08 17:24:40 +0900

    refactor: 게임 캐싱 비활성화

--- src/main/java/com/snackgame/server/config/CacheConfig.java
+++ src/main/java/com/snackgame/server/config/CacheConfig.java
@@ -4,16 +4,12 @@ import java.util.List;
 
 import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.caffeine.CaffeineCache;
 import org.springframework.cache.support.SimpleCacheManager;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 import com.github.benmanes.caffeine.cache.Caffeine;
 
-@Configuration
-@EnableCaching
 public class CacheConfig {
 
     @Bean
