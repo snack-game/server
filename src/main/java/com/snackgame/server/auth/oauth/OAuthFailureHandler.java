a447311bdab73f93145b8a0119fa074d2ac20449
0chil <0@chll.it>
2023-11-08 16:39:32 +0900

    refactor: OAuth 수행 후 리다이렉션 실패 로깅

--- src/main/java/com/snackgame/server/auth/oauth/OAuthFailureHandler.java
+++ src/main/java/com/snackgame/server/auth/oauth/OAuthFailureHandler.java
@@ -1,6 +1,7 @@
 package com.snackgame.server.auth.oauth;
 
 import java.io.IOException;
+import java.util.Collections;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -9,6 +10,9 @@ import javax.servlet.http.HttpSession;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class OAuthFailureHandler implements AuthenticationFailureHandler {
 
     @Override
@@ -31,7 +35,9 @@ public class OAuthFailureHandler implements AuthenticationFailureHandler {
     private String getRefererFrom(HttpSession session) {
         String referer = (String)session.getAttribute(SessionOAuthRequestStoringFilter.REFERER_ATTRIBUTE_NAME);
         if (referer == null) {
-            return "https://snackga.me/";
+            log.warn("Referer를 찾지 못했습니다. sessionAttributes= {}",
+                    Collections.list(session.getAttributeNames()).toArray());
+            return "https://snackga.me";
         }
         return referer;
     }
