14b0ee031fb2eced4066765d23a319d9b7f5cd60
Park - Jeonghwan <990706leo@gmail.com>
2024-01-18 15:14:50 +0900

    refactor: 트랜잭션 제거

--- src/main/java/com/snackgame/server/auth/token/domain/RefreshTokenRepository.java
+++ src/main/java/com/snackgame/server/auth/token/domain/RefreshTokenRepository.java
@@ -2,11 +2,9 @@ package com.snackgame.server.auth.token.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
-    @Transactional
     void deleteByToken(String token);
 }
