cd684ebb0c47b87b040cfc4ee9299f813f880753
Park - Jeonghwan <990706leo@gmail.com>
2024-02-01 17:13:26 +0900

    refactor: 리뷰 반영

--- src/main/java/com/snackgame/server/auth/token/support/JwtMemberArgumentResolver.java
+++ src/main/java/com/snackgame/server/auth/token/support/JwtMemberArgumentResolver.java
@@ -6,6 +6,8 @@ import java.util.Optional;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
@@ -28,6 +30,8 @@ public class JwtMemberArgumentResolver implements HandlerMethodArgumentResolver 
     private final JwtProvider accessTokenProvider;
     private final MemberResolver<?> memberResolver;
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Override
     public boolean supportsParameter(MethodParameter parameter) {
         return parameter.hasParameterAnnotation(Authenticated.class);
@@ -40,28 +44,34 @@ public class JwtMemberArgumentResolver implements HandlerMethodArgumentResolver 
             NativeWebRequest webRequest,
             WebDataBinderFactory binderFactory
     ) {
+
+        Optional<Cookie> foundCookie = getCookieFromRequest(webRequest);
+        if (foundCookie.isPresent()) {
+            String cookieToken = foundCookie.get().getValue();
+
+            return resolveMember(cookieToken);
+        }
+        String authorization = webRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        String headerToken = bearerTokenExtractor.extract(authorization);
+
+        return resolveMember(headerToken);
+    }
+
+    private Optional<Cookie> getCookieFromRequest(NativeWebRequest webRequest) {
         HttpServletRequest request = (HttpServletRequest)webRequest.getNativeRequest();
-
-        Long memberId = null ;
-
-        if(request.getCookies() != null) {
-            Optional<Cookie> foundCookie = Arrays.stream(request.getCookies())
+        if (request.getCookies() != null) {
+            return Arrays.stream(request.getCookies())
                     .filter(cookie -> cookie.getName().equals("accessToken"))
                     .findFirst();
-            if(foundCookie.isPresent()){
-                String cookieToken = foundCookie.get().getValue();
-                accessTokenProvider.validate(cookieToken);
-
-                memberId = Long.parseLong(accessTokenProvider.getSubjectFrom(cookieToken));
-            }
-
-        } else {
-            String authorization = webRequest.getHeader(HttpHeaders.AUTHORIZATION);
-            String headerToken = bearerTokenExtractor.extract(authorization);
-            accessTokenProvider.validate(headerToken);
-
-            memberId = Long.parseLong(accessTokenProvider.getSubjectFrom(headerToken));
         }
+        log.error("요청에 쿠키가 존재하지 않습니다.");
+        return Optional.empty();
+    }
+
+    private Object resolveMember(String token) {
+        accessTokenProvider.validate(token);
+
+        Long memberId = Long.parseLong(accessTokenProvider.getSubjectFrom(token));
         return memberResolver.resolve(memberId)
                 .orElseThrow(TokenAuthenticationException::new);
     }
