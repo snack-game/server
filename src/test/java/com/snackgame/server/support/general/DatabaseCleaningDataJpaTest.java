6895474dcab1b826470c3743615a26e302d82548
0chil <0@chll.it>
2024-02-15 22:38:12 +0900

    fix(test): DataJpaTest에서 테스트 픽스쳐 도구 사용

--- src/test/java/com/snackgame/server/support/general/DatabaseCleaningDataJpaTest.java
+++ src/test/java/com/snackgame/server/support/general/DatabaseCleaningDataJpaTest.java
@@ -8,13 +8,16 @@ import java.lang.annotation.Target;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.autoconfigure.jdbc.TestDatabaseAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import com.snackgame.server.support.database.CleanDatabaseBeforeEach;
+import com.snackgame.server.support.fixture.FixtureSaver;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @ExtendWith(CleanDatabaseBeforeEach.class)
 @DataJpaTest(excludeAutoConfiguration = TestDatabaseAutoConfiguration.class)
+@Import(FixtureSaver.class)
 public @interface DatabaseCleaningDataJpaTest {
 
 }
