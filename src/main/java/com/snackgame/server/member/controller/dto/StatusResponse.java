e743c4171fe8d2b3a70afecb79f171479badbf2d
0chil <0@chll.it>
2024-02-29 14:54:33 +0900

    feat: 현재 레벨의 최대경험치를 알 수 있다 (#106)
    * feat: 현재 레벨의 최대경험치를 알 수 있다
    
    * docs: 문서화

--- src/main/java/com/snackgame/server/member/controller/dto/StatusResponse.java
+++ src/main/java/com/snackgame/server/member/controller/dto/StatusResponse.java
@@ -2,6 +2,7 @@ package com.snackgame.server.member.controller.dto;
 
 import com.snackgame.server.member.domain.Status;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,13 +10,18 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class StatusResponse {
 
+    @Schema(example = "1")
     private final Long level;
-
+    @Schema(example = "200")
     private final Double exp;
+    @Schema(example = "240")
+    private final Double maxExp;
 
     public static StatusResponse of(Status status) {
         return new StatusResponse(
                 status.getLevel(),
-                status.getExp().doubleValue());
+                status.getExp().doubleValue(),
+                status.expRequiredForLevel().doubleValue()
+        );
     }
 }
