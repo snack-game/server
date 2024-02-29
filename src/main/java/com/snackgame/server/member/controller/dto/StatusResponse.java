7e3ffefcaaa929651a955de814ef6f1faedeaedb
0chil <0@chll.it>
2024-02-29 14:50:26 +0900

    docs: 문서화

--- src/main/java/com/snackgame/server/member/controller/dto/StatusResponse.java
+++ src/main/java/com/snackgame/server/member/controller/dto/StatusResponse.java
@@ -2,6 +2,7 @@ package com.snackgame.server.member.controller.dto;
 
 import com.snackgame.server.member.domain.Status;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
@@ -9,8 +10,11 @@ import lombok.RequiredArgsConstructor;
 @Getter
 public class StatusResponse {
 
+    @Schema(example = "1")
     private final Long level;
+    @Schema(example = "200")
     private final Double exp;
+    @Schema(example = "240")
     private final Double maxExp;
 
     public static StatusResponse of(Status status) {
