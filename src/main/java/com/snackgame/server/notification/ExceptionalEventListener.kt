4d9a7d384c6f7667af133b38faed587831d69eeb
0chil <0@chll.it>
2024-03-06 21:21:15 +0900

    feat: 예상치 못한 예외는 디스코드 알림을 전송한다

---
+++ src/main/java/com/snackgame/server/notification/ExceptionalEventListener.kt
@@ -0,0 +1,29 @@
+package com.snackgame.server.notification
+
+import com.snackgame.server.common.exception.event.ExceptionalEvent
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Profile
+import org.springframework.context.event.EventListener
+import org.springframework.scheduling.annotation.Async
+import org.springframework.stereotype.Component
+
+@Component
+open class ExceptionalEventListener(
+    @Value("\${spring.profiles.active}")
+    private val environment: String,
+    @Value("\${discord.system-webhook-url}")
+    private val systemWebhookUrl: String,
+    private val discordNotification: DiscordNotification
+) {
+
+    @Async
+    @EventListener
+    open fun onException(exceptionalEvent: ExceptionalEvent) {
+        discordNotification.send(systemWebhookUrl,
+            buildString {
+                appendLine("🚨 `$environment` 서버 예외 발생 🚨")
+                append("`${exceptionalEvent.exceptionName}`: `${exceptionalEvent.message}`")
+            }
+        )
+    }
+}
