c6b8c5b7e65361f092f4d2377c628e69e0d1fa24
0chil <0@chll.it>
2024-04-05 17:21:11 +0900

    fix: 개발 페이지를 CORS 허용한다

--- src/main/java/com/snackgame/server/config/WebMvcConfig.java
+++ src/main/java/com/snackgame/server/config/WebMvcConfig.java
@@ -44,6 +44,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .allowedOriginPatterns(
                         "https://" + HOST_NAME,
                         "https://api." + HOST_NAME,
+                        "https://dev." + HOST_NAME,
+                        "https://dev-api." + HOST_NAME,
                         "https://*snack-game.vercel.app",
                         "http://localhost:[*]"
                 )
