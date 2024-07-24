910c90df80912874c80064b99ee6300740a3b4f4
Park - Jeonghwan <990706leo@gmail.com>
2024-07-24 18:14:57 +0900

    test: 테스트 추가

--- src/test/java/com/snackgame/server/member/service/SocialMemberNameRandomizerTest.kt
+++ src/test/java/com/snackgame/server/member/service/SocialMemberNameRandomizerTest.kt
@@ -16,5 +16,19 @@ class SocialMemberNameRandomizerTest {
         assertThat(randomized).endsWith("_스낵이")
     }
 
+    @Test
+    fun `이름은 정해둔 배열에 존재하는 단어만을 가지고 생성되어야한다`() {
+        var randomizedPureWord = socialMemberNameRandomizer.getName().string.removeSuffix("_스낵이")
+
+        var list: MutableList<String> = mutableListOf()
+        randomizedPureWord.split("_").forEach {
+            list.add(it)
+        }
+
+        assertThat(SocialMemberNameRandomizer.determiners).contains(list[0])
+        assertThat(SocialMemberNameRandomizer.adjectives).contains(list[1])
+
+    }
+
 
 }
