194be5ce3bc40536668dd1564f3e45adc5556955
0chil <0@chll.it>
2024-10-16 18:27:09 +0900

    feat(messaging): 한 기기는 한 계정당 한 번만 등록할 수 있다

--- src/main/java/com/snackgame/server/messaging/notification/domain/DeviceRepository.kt
+++ src/main/java/com/snackgame/server/messaging/notification/domain/DeviceRepository.kt
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 
 interface DeviceRepository : JpaRepository<Device, Long> {
 
+    fun existsByOwnerIdAndToken(ownerId: Long, token: String): Boolean
+
     fun findAllByOwnerId(ownerId: Long): List<Device>
 }
--- src/main/java/com/snackgame/server/messaging/notification/service/NotificationService.kt
+++ src/main/java/com/snackgame/server/messaging/notification/service/NotificationService.kt
@@ -2,6 +2,7 @@ package com.snackgame.server.messaging.notification.service
 
 import com.snackgame.server.messaging.notification.domain.Device
 import com.snackgame.server.messaging.notification.domain.DeviceRepository
+import com.snackgame.server.messaging.notification.exception.DuplicatedDeviceException
 import com.snackgame.server.messaging.notification.service.dto.DeviceResponse
 import com.snackgame.server.messaging.notification.service.dto.DeviceTokenRequest
 import org.springframework.stereotype.Service
@@ -10,8 +11,12 @@ import org.springframework.stereotype.Service
 class NotificationService(private val deviceRepository: DeviceRepository) {
 
     fun registerDeviceFor(ownerId: Long, deviceToken: DeviceTokenRequest) {
-        Device(ownerId, deviceToken.token)
-            .let { deviceRepository.save(it) }
+        if (!deviceRepository.existsByOwnerIdAndToken(ownerId, deviceToken.token)) {
+            Device(ownerId, deviceToken.token)
+                .let { deviceRepository.save(it) }
+            return
+        }
+        throw DuplicatedDeviceException()
     }
 
     fun getDevicesOf(ownerId: Long): List<DeviceResponse> {
