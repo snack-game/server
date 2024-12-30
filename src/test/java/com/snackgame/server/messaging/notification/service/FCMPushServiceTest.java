2950ed12b59de5f0308629da26615964e5db5a7e
Park - Jeonghwan <990706leo@gmail.com>
2024-12-30 19:26:02 +0900

    test: FCM을 통해 Multicast를 할 수 있다

---
+++ src/test/java/com/snackgame/server/messaging/notification/service/FCMPushServiceTest.java
@@ -0,0 +1,43 @@
+package com.snackgame.server.messaging.notification.service;
+
+import static com.snackgame.server.member.fixture.MemberFixture.정환;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.snackgame.server.member.fixture.MemberFixture;
+import com.snackgame.server.messaging.notification.service.dto.DeviceTokenRequest;
+import com.snackgame.server.support.general.ServiceTest;
+
+@ServiceTest
+public class FCMPushServiceTest {
+
+    @Autowired
+    private FCMPushService fcmPushService;
+
+    @Autowired
+    private NotificationService notificationService;
+
+    @BeforeEach
+    void setUp() {
+        MemberFixture.saveAll();
+    }
+
+    @DisplayName("토큰을 통해 푸시알림을 전송할 수 있다")
+    @Test
+    void sendMessage() throws ExecutionException, InterruptedException {
+        notificationService.registerDeviceFor(정환().getId(),
+                new DeviceTokenRequest(
+                        "cFwP3VYHh0eyoXkyY9MwJr:APA91bHX5YiVXuIvi-pLDqNHcJMhl7hKrqLTC7opFMbzj4CsXrg1wu2ayG_LFVREto678gQdWGUnmBXwKEpEJTfXheX0Fz83xwqDzVrKvXF3H5t07XXU6e-boq8JnZVCbs6NB_VfGRh8"));
+
+        Future<?> future = fcmPushService.sendPushMessage("테스트", "테스트", 정환().getId());
+
+        future.get();
+    }
+
+}
