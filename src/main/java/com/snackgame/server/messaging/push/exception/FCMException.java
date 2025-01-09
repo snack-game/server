3d73f0dd55d7cd06e84e392021d8fe97fc71d25e
Park - Jeonghwan <990706leo@gmail.com>
2025-01-09 16:41:59 +0900

    refactor: 에러메세지 수정

--- src/main/java/com/snackgame/server/messaging/push/exception/FCMException.java
+++ src/main/java/com/snackgame/server/messaging/push/exception/FCMException.java
@@ -5,7 +5,7 @@ import com.snackgame.server.common.exception.Kind;
 public class FCMException extends NotificationException {
 
     public FCMException() {
-        super("잘못된 FCM 접근입니다.", Kind.BAD_REQUEST);
+        super("잘못된 FCM 접근입니다.", Kind.INTERNAL_SERVER_ERROR);
     }
 
 }
