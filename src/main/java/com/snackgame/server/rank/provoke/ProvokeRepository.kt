f5af9728964416f62ccbbb755f3ad6d463dc4b79
Park - Jeonghwan <990706leo@gmail.com>
2025-03-21 14:10:38 +0900

    fix: 예외처리 추가 (#202)

--- src/main/java/com/snackgame/server/rank/provoke/ProvokeRepository.kt
+++ src/main/java/com/snackgame/server/rank/provoke/ProvokeRepository.kt
@@ -1,8 +1,9 @@
 package com.snackgame.server.rank.provoke
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.*
 
 interface ProvokeRepository : JpaRepository<Provoke, Long> {
 
-    fun findProvokeByOwnerIdAndSessionId(ownerId: Long, sessionId: Long): Provoke
+    fun findProvokeByOwnerIdAndSessionId(ownerId: Long, sessionId: Long): Optional<Provoke>
 }
