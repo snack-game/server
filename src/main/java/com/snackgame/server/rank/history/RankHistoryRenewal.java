6998b8d8590aff527217ffe104c5237384c49990
Park - Jeonghwan <990706leo@gmail.com>
2025-03-28 17:12:28 +0900

    fix: 등수 갱신없는 상황에 도발이 예약되었던 문제 해결

--- src/main/java/com/snackgame/server/rank/history/RankHistoryRenewal.java
+++ src/main/java/com/snackgame/server/rank/history/RankHistoryRenewal.java
@@ -24,13 +24,12 @@ public class RankHistoryRenewal {
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void renewHistoryWith(BestScoreRenewalEvent event) {
-
         RankHistory rankHistory = getOrCreateRankHistoryBy(event);
         if (rankHistory.canRenewBy(event.getRenewedRank())) {
-            /// TODO: 1/16/25 save와 update가 필요한게 같은데 어떻게 잘 합쳐볼수없을까
             rankHistories.save(rankHistory.renewWith(event.getOwnerId(), event.getRenewedRank()));
+            updateAndReserve(event);
         }
-        updateAndReserve(event);
+
     }
 
     private void updateAndReserve(BestScoreRenewalEvent event) {
