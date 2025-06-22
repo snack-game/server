a29f3909865f6e03fa9ec407c80697b97f40624d
hwanvely <990706leo@gmail.com>
2025-06-23 08:49:49 +0900

    feat: 아이템을 사용할 수 있다

--- src/main/java/com/snackgame/server/game/snackgame/core/domain/item/ItemRepository.kt
+++ src/main/java/com/snackgame/server/game/snackgame/core/domain/item/ItemRepository.kt
@@ -1,8 +1,10 @@
 package com.snackgame.server.game.snackgame.core.domain.item
 
 import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
 
 interface ItemRepository : JpaRepository<Item, Long> {
 
     fun findAllByOwnerId(ownerId: Long): List<Item>
+    fun findItemByOwnerIdAndItemType(ownerId: Long, itemType: ItemType) : Optional<Item>
 }
