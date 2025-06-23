77265cd399e9db6fc52af14aecbb9efd61a831f8
hwanvely <990706leo@gmail.com>
2025-06-23 09:24:49 +0900

    fix: 좌표는 음수가 될 수 없다

--- src/main/java/com/snackgame/server/game/snackgame/core/domain/Coordinate.kt
+++ src/main/java/com/snackgame/server/game/snackgame/core/domain/Coordinate.kt
@@ -18,7 +18,12 @@ class Coordinate(val y: Int, val x: Int) {
     }
 
     fun toBombCoordinate(): List<Coordinate> {
-        return AROUND_OFFSETS.map { (dy, dx) -> Coordinate(y + dy, x + dx) }
+        return AROUND_OFFSETS
+            .mapNotNull { (dy, dx) ->
+                val newY = y + dy
+                val newX = x + dx
+                if (newY >= 0 && newX >= 0) Coordinate(newY, newX) else null
+            }
     }
 
     override fun equals(o: Any?): Boolean {
@@ -43,11 +48,12 @@ class Coordinate(val y: Int, val x: Int) {
                 ", x=" + x +
                 '}'
     }
+
     companion object {
         val AROUND_OFFSETS = listOf(
             -1 to -1, -1 to 0, -1 to 1,
-            0 to -1,  0 to 0,  0 to 1,
-            1 to -1,  1 to 0,  1 to 1
+            0 to -1, 0 to 0, 0 to 1,
+            1 to -1, 1 to 0, 1 to 1
         )
     }
 }
