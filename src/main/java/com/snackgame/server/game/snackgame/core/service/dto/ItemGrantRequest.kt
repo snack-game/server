11da2d8489697017625d6cda6c42786f0e72630c
Park - Jeonghwan <990706leo@gmail.com>
2025-07-10 07:43:05 +0900

    아이템 지급 검증 에러 해결 (#218)
    * feat: 사용자의 아이템 보유 여부를 확인할 수 있다.
    
    * feat: Type입력 시 Enum으로의 변환을 위해 소문자를 대비한다.
    
    * refactor: 메서드 네이밍 수정
    
    * refactor: 보유한 아이템 수를 반환하도록 수정
    
    * refactor: 테스트 픽스처 네이밍 수정
    
    * feat: 아이템 조회 테스트 작성
    
    * feat: 폭탄과 일반 스트릭 분리
    
    * feat: 폭탄과 일반 스트릭 분리
    
    * feat: 폭탄을 통해 스낵을 제거할 수 있다
    
    * feat: 아이템을 사용할 수 있다
    
    * feat: 폭탄이 터질 좌표를 만들 수 있다
    
    * fix: 좌표는 음수가 될 수 없다
    
    * feat: 폭탄 아이템을 사용할 수 있다
    
    * feat: 폭탄으로 황금 스낵을 제거하면 보드가 초기화된다
    
    * feat: 피버타임을 사용할 수 있다
    
    * refactor: 메서드 네이밍 수정
    
    * docs: API 문서 작성
    
    * fix: 폭탄에 빈 스낵이 포함되어도 점수를 얻을 수 없다
    
    * feat: 아이템을 수동으로 지급할 수 있다
    
    * feat: 아이템 조회 시 발급받은 적이 없다면 모든 아이템을 새로 만들어서 제공한다
    
    * refactor: 아이템 개수 변경을 도메인 내에서 처리하도록 수정
    
    * feat: 아이템의 지급 정책에 따라 검증할 수 있다
    
    * fix: non-ascii warning 해결
    
    * fix: 테이블 이름 수정
    
    * fix: requestbody 중복 해결

--- src/main/java/com/snackgame/server/game/snackgame/core/service/dto/ItemGrantRequest.kt
+++ src/main/java/com/snackgame/server/game/snackgame/core/service/dto/ItemGrantRequest.kt
@@ -3,9 +3,12 @@ package com.snackgame.server.game.snackgame.core.service.dto
 import com.fasterxml.jackson.annotation.JsonCreator
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.snackgame.server.game.snackgame.core.domain.item.ItemType
+import com.snackgame.server.game.snackgame.core.domain.item.policy.GrantType
 
 
-data class ItemTypeRequest @JsonCreator constructor(
+data class ItemGrantRequest @JsonCreator constructor(
     @JsonProperty("itemType")
-    val itemType: ItemType
+    val itemType: ItemType,
+    @JsonProperty("grantType")
+    val grantType: GrantType
 )
