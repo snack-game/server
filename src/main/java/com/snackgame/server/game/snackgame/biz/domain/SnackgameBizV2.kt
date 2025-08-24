a76212d10fc29f1e0b413320325e8167669c3108
hwanvely <990706leo@gmail.com>
2025-08-25 06:19:20 +0900

    refactor: 바뀐 dto에 맞게 BIZ 및 test 수정

--- src/main/java/com/snackgame/server/game/snackgame/biz/domain/SnackgameBizV2.kt
+++ src/main/java/com/snackgame/server/game/snackgame/biz/domain/SnackgameBizV2.kt
@@ -6,12 +6,17 @@ import com.snackgame.server.game.snackgame.core.domain.Board
 import com.snackgame.server.game.snackgame.core.domain.BoardConverter
 import com.snackgame.server.game.snackgame.core.domain.Snackgame.Companion.DEFAULT_HEIGHT
 import com.snackgame.server.game.snackgame.core.domain.Snackgame.Companion.DEFAULT_WIDTH
+import com.snackgame.server.game.snackgame.core.domain.Snackgame.Companion.FEVER_MULTIPLIER
+import com.snackgame.server.game.snackgame.core.domain.Snackgame.Companion.NORMAL_MULTIPLIER
 import com.snackgame.server.game.snackgame.core.domain.Snackgame.Companion.SESSION_TIME
 import com.snackgame.server.game.snackgame.core.domain.Snackgame.Companion.SPARE_TIME
 import com.snackgame.server.game.snackgame.core.domain.Streak
+import com.snackgame.server.game.snackgame.core.domain.item.FeverTime
 import com.snackgame.server.game.snackgame.core.domain.snack.Snack
+import com.snackgame.server.game.snackgame.core.service.dto.StreakWithFever
 import java.time.Duration
 import javax.persistence.Convert
+import javax.persistence.Embedded
 import javax.persistence.Entity
 import javax.persistence.Lob
 
@@ -28,12 +33,36 @@ open class SnackgameBizV2(
     var board = board
         private set
 
+    @Embedded
+    var feverTime: FeverTime? = null
+        private set
+
     fun remove(streak: Streak) {
         val removedSnacks = board.removeSnacksIn(streak)
         this.score += removedSnacks.size
         if (removedSnacks.any(Snack::isGolden)) {
             this.board = board.reset()
         }
+    }
+
+    fun remove(streakWithFever: StreakWithFever) {
+        val streak = streakWithFever.streak
+        val removedSnacks = board.removeSnacksIn(streak)
+
+        val serverIsFever = feverTime?.isActive(streakWithFever.occurredAt) == true
+        val isValid = streakWithFever.clientIsFever && serverIsFever
+
+        val multiplier = if (isValid) FEVER_MULTIPLIER else NORMAL_MULTIPLIER
+        increaseScore(streak.length * multiplier)
+
+        if (removedSnacks.any(Snack::isGolden)) {
+            this.board = board.reset()
+        }
+    }
+
+    private fun increaseScore(earn: Int) {
+        val multiplier = if (feverTime?.isActive() == true) FEVER_MULTIPLIER else NORMAL_MULTIPLIER
+        this.score += earn * multiplier
     }
 
     override val metadata = SNACK_GAME_BIZ_V2
